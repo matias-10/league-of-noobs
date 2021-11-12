cf9012ccb30706d2e503d7962813c0f51fbf49e3
Machi-Chen <74550174+Machi-Chen@users.noreply.github.com>
2021-11-12 13:12:56 -0500

    Update unit.py

--- league/unit.py
+++ league/unit.py
@@ -12,10 +12,10 @@ class Stats:
 
 class Unit:
     def __init__(self, base_health: int, base_damage: int, base_armor: int, base_range: int, base_speed: int, team: Team, enemy_team: Team) -> None:
-        self.base_stats = Stats(base_health, base_damage, base_armor, base_range, base_speed)
+        self._base_stats = Stats(base_health, base_damage, base_armor, base_range, base_speed)
         self._level = 1
-        self.team = team
-        self.enemy_team = enemy_team
+        self._team = team
+        self._enemy_team = enemy_team
 
     def apply_debuff(self, debuff: Ability):
         "Take an Ability object and stores the debuff"
