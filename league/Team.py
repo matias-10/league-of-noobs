22db592f37b36e6208fc2583b943498e1d980266
matias-10 <71330656+matias-10@users.noreply.github.com>
2021-11-22 09:25:03 -0500

    Create Team.py

---
+++ league/Team.py
@@ -0,0 +1,22 @@
+class Team:
+    def __init__(self, nexus, Minions: list[minion], towers: list[towers], heroes: list[hero]) -> None:
+        self.nexus = nexus
+        self.Minions = Minions
+        self.towers = towers
+        self.heroes = heroes
+    def spawn_minions():
+        pass
+    def get_minions():
+       pass
+    def get_towers():
+        pass
+    def get_nexus():
+        pass
+    def get_all():
+        pass
+    def create_blue_team(heroes: list[hero]):
+        pass
+    def create_red_team(heroes: list[hero]):
+        pass
+      
+     
