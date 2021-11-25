da59b71cf8a4e03a884d470a05110c8cfbd67add
matias-10 <71330656+matias-10@users.noreply.github.com>
2021-11-24 22:08:16 -0500

    Update team.py

--- league/team.py
+++ league/team.py
@@ -1,2 +1,37 @@
+from typing import List, Tuple
+from unit import Hero, Tower, Nexus, Minion
+
+
 class Team:
-    pass
+    def __init__(self, heroes: List[Hero], nexus_point: Tuple[int, int], tower_points: List[Tuple[int, int]]) -> None:
+        self.heroes = heroes
+        self.nexus = None
+        # self._towers = None
+        # self.minions = None
+        # self._minions: List[Minion] = []
+        # self._towers : List[Tower] = []
+    def get_towers(self) -> List[Tower]:
+        return self.towers
+    
+    def get_nexus(self) -> Nexus:
+       return self.nexus
+    
+    def get_all(self): #Concatonate lists
+        all = self._towers + self.heroes + self.nexus
+        return all
+    
+    def _spawn_towers(tower_points :List[Tuple[int, int]]):
+        pass
+
+    def _sparwn_minions()-> None:
+        pass
+
+    @staticmethod
+    def create_blue_team(heroes: List[Hero]) -> "Team":
+        pass
+    
+    @staticmethod
+    def create_red_team( heroes: List[Hero]) -> "Team":
+        pass
+
+
